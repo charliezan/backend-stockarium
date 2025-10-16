f9c341db67cd1412059a47eb19a4b0dd710186df
Alessio Sosa <alessio.sosa.2005@gmail.com>
2025-10-16 09:52:30 -0300

    primer commit

---
+++ src/main/java/fantastic4/stockariumbackend/StockariumBackendApplication.java
@@ -0,0 +1,13 @@
+package fantastic4.stockariumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockariumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockariumBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fantastic4/stockariumbackend/StockariumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fantastic4.stockariumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockariumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
